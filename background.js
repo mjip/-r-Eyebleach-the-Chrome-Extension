99c1ac757599aea50f860276f6cc92dbac111ba3
enteralienships <syt.zhang@gmail.com>
2017-01-29 00:37:50 -0500

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-var totalCount = 0;
-var threshold = 15;
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
-  totalCount = totalCount + response;
-  if ( totalCount > threshold ){
-    //alert("PANIC");
-  }
-})
