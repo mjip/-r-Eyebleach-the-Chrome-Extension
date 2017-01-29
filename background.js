c43e177eab823befe5c80df7feee086a67e2f3f6
enteralienships <syt.zhang@gmail.com>
2017-01-29 00:15:39 -0500

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-var count = 0;
-chrome.runtime.onMessage.addListener(function(message, sender, function sendResponse){
-  count += message;
-  if (count > 2){
-    alert("PANIC!!");
-  } 
-  alert("PANIC!!!!!!!");
-});
-alert("PANIC!!!!!!!");
