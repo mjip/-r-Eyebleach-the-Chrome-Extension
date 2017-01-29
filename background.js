8fa18ef630bad53761ea51b73037bdcbb573f3b0
Marie Payne <marie.payne@hotmail.ca>
2017-01-29 10:03:16 -0500

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-var totalCount = 0;
-var threshold = 1269;
-
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
-  totalCount = totalCount + response;
-  if ( totalCount > threshold ){
-    chrome.tabs.create({url : "http://i.imgur.com/XxiaNNo.gifv"});
-  }
-})
