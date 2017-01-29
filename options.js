5bbe39b823f51bd4e8e6d435fb7acb25ca9be767
Marie Payne <marie.payne@hotmail.ca>
2017-01-29 13:52:39 -0500

    Delete options.js

--- options.js
+++
@@ -1,9 +0,0 @@
-function save_options() {
-    var theValue = document.getElementById('filters').value;
-    chrome.storage.sync.set({'filters': theValue}, function(){
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-    });
-}
-
-document.getElementById('submit').addEventListener('click', save_options);
